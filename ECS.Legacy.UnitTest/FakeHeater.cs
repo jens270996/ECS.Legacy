2317ee54fe9e30fb64261e507c7712eb3f9cfeca
Jens Kristensen <jens-nk@hotmail.com>
2020-09-17 13:18:02 +0200

    Fakes

---
+++ ECS.Legacy.UnitTest/FakeHeater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ECS.Legacy.UnitTest
+{
+    class FakeHeater:IHeater
+    {
+       public bool SelfTest { get; set; }
+        
+        public void TurnOn()
+        {
+            
+        }
+
+        public void TurnOff()
+        {
+            
+        }
+
+        public bool RunSelfTest()
+        {
+            return SelfTest;
+        }
+    }
+}
---
+++ ECS.Legacy.UnitTest/FakeTempSensor.cs
@@ -0,0 +1,18 @@
+﻿namespace ECS.Legacy.UnitTest
+{
+    class FakeTempSensor:ITempSensor
+    {
+        public int Temp { get; set; }
+        public bool SelfTest { get; set; }
+
+        public int GetTemp()
+        {
+            return Temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return SelfTest;
+        }
+    }
+}
