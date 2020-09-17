71f895bb757a795a87832f342e7a974209a10a35
Jens Kristensen <jens-nk@hotmail.com>
2020-09-17 12:42:11 +0200

    Fixed Design

---
+++ ECS.Legacy/IHeater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    public interface IHeater
+    {
+        public void TurnOn();
+        public void TurnOff();
+        public bool RunSelfTest();
+
+    }
+}
