4fd4c199093c5bd66d11b2f6f3629b6268866abe
Joachim <Kroyer.joachim@gmail.com>
2020-09-17 13:55:37 +0200

    Tilføjede test fil og setup

---
+++ ECS.Legacy.UnitTest/ECS.Legacy.UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ECS.Legacy;
+
+namespace ECS.Legacy.UnitTest
+{
+    public class ECSLegacyUnitTest
+    {
+        ECS uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            FakeHeater _fakeHeater = new FakeHeater();
+            FakeTempSensor _fakeTempSensor = new FakeTempSensor();
+            int threshold = 28;
+            uut = new ECS(threshold, _fakeTempSensor, _fakeHeater); 
+
+            // Test of ECS
+
+            [Test]
+            
+
+            // Test of TempSensor
+
+            // Test of Heater
+        }
+    }
+}
