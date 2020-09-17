fc9ac7fac88666b91f4f35bf4dcb6e8c058f23ef
Jens Kristensen <jens-nk@hotmail.com>
2020-09-17 14:10:52 +0200

    First Test

--- ECS.Legacy.UnitTest/ECS.Legacy.UnitTest.cs
+++ ECS.Legacy.UnitTest/ECS.Legacy.UnitTest.cs
@@ -8,23 +8,31 @@ namespace ECS.Legacy.UnitTest
 {
     public class ECSLegacyUnitTest
     {
-        ECS uut;
-
+        private ECS uut;
+        private FakeTempSensor _fakeTempSensor;
+        private FakeHeater _fakeHeater;
         [SetUp]
         public void SetUp()
         {
-            FakeHeater _fakeHeater = new FakeHeater();
-            FakeTempSensor _fakeTempSensor = new FakeTempSensor();
+            _fakeHeater = new FakeHeater();
+            _fakeTempSensor = new FakeTempSensor();
             int threshold = 28;
             uut = new ECS(threshold, _fakeTempSensor, _fakeHeater); 
 
-            // Test of ECS
-
             
-
-            // Test of TempSensor
-
-            // Test of Heater
         }
+        // Test of ECS
+        [TestCase(9,7, "Turned On\n")]
+        public void Regulate_CorrectLogRes(int thresh, int temp,string logRes)
+        {
+            uut.SetThreshold(thresh);
+            _fakeTempSensor.Temp = temp;
+
+            uut.Regulate();
+            Assert.That(_fakeHeater.Log.Log,Is.EqualTo(logRes));
+        }
+
+
+        
     }
 }
